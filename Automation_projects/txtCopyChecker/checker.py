87aa79778f1db056116c974ab6c957c89e57e402
Luis Felipe Do Carmo Costa Tavares <48478593+LuisFelipeTs@users.noreply.github.com>
2022-01-25 16:48:21 -0300

    Add files via upload

--- Automation_projects/txtCopyChecker/checker.py
+++ Automation_projects/txtCopyChecker/checker.py
@@ -1,2 +1,34 @@
+
+import requests
+from parsel import Selector
+import re
+import pickle
+
+class Textr(object):
+    def __init__(self, text, desc):
+        self.text = text
+        self.desc = desc
+
+    @property
+    def chave(self):
+        return self.text
+
+    def __str__(self):
+        return '{0} {1} {0}\n{2}\n'.format('*' * 10, self.text, self.desc)
+
 def checkText(text_to_check):
-    return
+    link_r = 'https://www.google.com.br/search?q=' + text_to_check
+    r = requests.get(link_r)
+    sel = Selector(r.text)
+    text_parts = sel.xpath('')
+    text = ' '.join(text_parts)
+    print(text)
+    text_t = re.sub('\s+', ' ', text, re.UNICODE)
+    letra = Textr(text_to_check , text_t)
+
+    print(letra)
+
+
+    #return n_output, validation
+
+checkText("Queijo")
