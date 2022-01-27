96433f3f68bfa05aa234ecc42ccbeabf321b200a
Luís Felipe Do Carmo Costa Tavares <48478593+LuisFelipeTs@users.noreply.github.com>
2022-01-27 16:30:03 -0300

    update to v1.1 (first return)

--- Automation_projects/txtCopyChecker/checker.py
+++ Automation_projects/txtCopyChecker/checker.py
@@ -5,30 +5,41 @@ import re
 import pickle
 
 class Textr(object):
-    def __init__(self, text, desc):
+    bol = True
+
+    def __init__(self, text):
         self.text = text
-        self.desc = desc
+        if text == "": self.val = False
+        else: self.val = True 
 
     @property
     def chave(self):
         return self.text
-
+   
     def __str__(self):
         return '{0} {1} {0}\n{2}\n'.format('*' * 10, self.text, self.desc)
 
-def checkText(text_to_check):
-    link_r = 'https://www.google.com.br/search?q=' + text_to_check
-    r = requests.get(link_r)
+def checkText_wk(text_to_check):
+    link_w = 'https://pt.wikipedia.org/wiki/' + text_to_check
+    r = requests.get(link_w)
     sel = Selector(r.text)
-    text_parts = sel.xpath('')
-    text = ' '.join(text_parts)
-    print(text)
-    text_t = re.sub('\s+', ' ', text, re.UNICODE)
-    letra = Textr(text_to_check , text_t)
+    text_parts = sel.xpath('//*[@id="mw-content-text"]/div[1]/p[1]//text()').extract()
+    #//span[@class="hgKElc"]//text()[last()]
+    textt = ' '.join(text_parts)
+    text_t = re.sub('\s', ' ', textt, re.UNICODE)
+    letra = Textr(text_t )
+    
+    return letra.text, letra.val
 
-    print(letra)
-
-
-    #return n_output, validation
-
-checkText("Queijo")
+def checkText_dc(text_to_check):
+    link_w = 'https://www.dicio.com.br/' + text_to_check
+    r = requests.get(link_w)
+    sel = Selector(r.text)
+    text_parts = sel.xpath('//*[@id="content"]/div[1]/div[1]/p/span[2]/text()').extract()
+    #//span[@class="hgKElc"]//text()[last()]
+    textt = ' '.join(text_parts)
+    text_t = re.sub('\s', ' ', textt, re.UNICODE)
+    letra = Textr(text_t )
+    
+    return letra.text, letra.val
+#print(checkText("bola")[1])
--- Automation_projects/txtCopyChecker/entryBox.py
+++ Automation_projects/txtCopyChecker/entryBox.py
@@ -1,8 +1,16 @@
 from tkinter import *
+from wsgiref import validate
+from checker import checkText_wk , checkText_dc
+import time
 
 def boxE(user_imput, calc_output):
+    calc_output.delete("1.0", "end")
     INPUT = user_imput.get("1.0", "end-1c")
-    if(INPUT == "120"):
-        calc_output.insert(END, 'Correct')
+    u_imp = str(INPUT)
+    output_txt, validation = checkText_wk(u_imp)
+    
+    if(validation):
+        calc_output.insert(END, output_txt)
     else:
-        calc_output.insert(END, "Wrong answer")
+        calc_output.insert(END, "The Word that was added is incorrect or doesn't exists")
+    
