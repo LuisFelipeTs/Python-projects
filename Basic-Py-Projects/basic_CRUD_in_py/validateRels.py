81aeef15736a4f25641b6b9c26785631ead05ccd
Luís Felipe Do Carmo Costa Tavares <48478593+LuisFelipeTs@users.noreply.github.com>
2022-02-08 11:59:57 -0300

    update validateRels

--- Basic-Py-Projects/basic_CRUD_in_py/validateRels.py
+++ Basic-Py-Projects/basic_CRUD_in_py/validateRels.py
@@ -1,3 +1,32 @@
+from operator import index
 import User
 import pandas as pd
 
+user_data = pd.read_excel("user_data.xlsx")
+
+def checkIfesxists(user):
+    if user not in list(user_data["username"]): return False
+    else: return True
+
+def getLogin(username, password):
+    if checkIfesxists(username):
+        ind = index(user_data["username"][username])
+        if user_data["password"][ind] == password:
+            index_ = user_data["user_id"][ind]
+            return True, index_
+        else: return False, "p"
+    else: return False, "u"
+
+def callUserinbase(user_id):
+    name = user_data["name"][user_id]
+    username = user_data["username"][user_id]
+    password = user_data["password"][user_id]
+    ux = name = user_data["UX"][user_id]
+    return User(user_id, name, username, password, ux)
+
+
+    
+
+
+#user_data = pd.read_excel("user_data.xlsx")
+#print(list(user_data["username"]))
