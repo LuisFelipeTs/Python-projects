8c2e7d5c91418f57cf1ed571cf325b5c6661926f
Luís Felipe Do Carmo Costa Tavares <48478593+LuisFelipeTs@users.noreply.github.com>
2022-02-12 15:13:21 -0300

    updating the gui_

--- Basic-Py-Projects/basic_CRUD_in_py/gui_.py
+++ Basic-Py-Projects/basic_CRUD_in_py/gui_.py
@@ -11,9 +11,10 @@ def tkinterBox(screen, session_id = 0):
     line= Label(text = "-----------------------------------")
 
     if screen == "login":
-        tk_screen.geometry("240x225" )
+        tk_screen.geometry("240x255" )
         tk_screen.title("Login C.R.U.D")
-        lab = Label(text = "Login:",font =("Sans-serif", 10))
+        lab_title = Label(text = "Logar-se",font =("Sans-serif", 13))
+        lab = Label(text = "Username:",font =("Sans-serif", 10))
         user_imput_lg = Text(tk_screen, height= 1, width= 20, bg= 'white')
         lab1 = Label(text = "Password:",font =("Sans-serif", 10))
         user_imput_ps = Text(tk_screen, height= 1, width= 20, bg= 'white')
@@ -22,7 +23,8 @@ def tkinterBox(screen, session_id = 0):
                  borderwidth = 3,
                  text ="Login!",
                  command = lambda:
-                 [loginBtt(user_imput_lg, user_imput_ps), checkIflog(log_r, tk_screen)]
+                 [closeScreen(tk_screen), tkinterBox("menu")]
+                 #loginBtt(user_imput_lg, user_imput_ps), checkIflog(log_r, tk_screen)
                  )
         btt_rg = Button(tk_screen,
                  borderwidth = 0,
@@ -35,13 +37,14 @@ def tkinterBox(screen, session_id = 0):
                  command = lambda:
                  closeScreen(tk_screen))
         log_r = Label(text = "",font =("Sans-serif", 15))
-        widgets_list = [log_r, lab, user_imput_lg, lab1, user_imput_ps, line, btt_lg, btt_rg, btt_ex]
+        widgets_list = [ log_r, lab_title, lab, user_imput_lg, lab1, user_imput_ps, line, btt_lg, btt_rg, btt_ex]
         callScreen(widgets_list, tk_screen)
 
     elif screen == "regis":
         tk_screen.geometry("260x285" )
         tk_screen.title("Registre-se C.R.U.D")
-        r_lab = Label(text = "Nome:",font =("Sans-serif", 10))
+        r_lab_title = Label(text = "Register",font =("Sans-serif", 13))
+        r_lab = Label(text = "Name:",font =("Sans-serif", 10))
         r_user_imput_nm = Text(tk_screen, height= 1, width= 20, bg= 'white')
         r_lab1 = Label(text = "Username:",font =("Sans-serif", 10))
         r_user_imput_us = Text(tk_screen, height= 1, width= 20, bg= 'white')
@@ -51,17 +54,75 @@ def tkinterBox(screen, session_id = 0):
         r_user_imput_cpass = Text(tk_screen, height= 1, width= 20, bg= 'white')
         r_btt_cd = Button(tk_screen,
                  borderwidth = 3,
-                 text ="Cadastrar-se!",
+                 text ="Register",
                  command = lambda:
                  [loginBtt(user_imput_lg, user_imput_ps), checkIflog(log_r, tk_screen)]
                  )
         r_btt_back = Button(tk_screen,
                  borderwidth = 0,
-                 text ="Voltar ao login.",
+                 text ="back to the login screen.",
                  command = lambda:
                  [closeScreen(tk_screen), tkinterBox("login")])
         r_log_r = Label(text = "",font =("Sans-serif", 15))
-        widgets_list_reg = [r_log_r, r_lab, r_user_imput_nm, r_lab1, r_user_imput_us, r_lab2, r_user_imput_pass , r_lab3, r_user_imput_cpass , line, r_btt_cd, r_btt_back]
+        widgets_list_reg = [ r_log_r, r_lab_title, r_lab, r_user_imput_nm, r_lab1, r_user_imput_us, r_lab2, r_user_imput_pass , r_lab3, r_user_imput_cpass , line, r_btt_cd, r_btt_back]
+        callScreen(widgets_list_reg, tk_screen)
+
+    elif screen == "menu":
+        tk_screen.geometry("260x285" )
+        tk_screen.title("Menu C.R.U.D")
+        m_lab_title = Label(text = "Menu",font =("Sans-serif", 13))
+        see_whoin_btt = Button(tk_screen,
+                 borderwidth = 1,
+                 text ="Show Users",
+                 command = lambda:
+                 [loginBtt(user_imput_lg, user_imput_ps), checkIflog(log_r, tk_screen)]
+                 )
+        config_btt = Button(tk_screen,
+                 borderwidth = 1,
+                 text ="Configurations",
+                 command = lambda:
+                 [closeScreen(tk_screen), tkinterBox("login")])
+        back_btt = Button(tk_screen,
+                 borderwidth = 1,
+                 text ="Go back",
+                 command = lambda:
+                 [loginBtt(user_imput_lg, user_imput_ps), checkIflog(log_r, tk_screen)]
+                 )
+        exit_btt = Button(tk_screen,
+                 borderwidth = 2,
+                 text ="Exit",
+                 command = lambda:
+                 [closeScreen(tk_screen), tkinterBox("login")])
+        widgets_list_reg = [m_lab_title, see_whoin_btt, config_btt, back_btt, line, exit_btt]
+        callScreen(widgets_list_reg, tk_screen)
+    
+    elif screen == "see_user":
+        tk_screen.geometry("260x285" )
+        tk_screen.title("Read C.R.U.D")
+        m_lab_title = Label(text = "Users",font =("Sans-serif", 13))
+        see_whoin_btt = Button(tk_screen,
+                 borderwidth = 1,
+                 text ="Show Users",
+                 command = lambda:
+                 [loginBtt(user_imput_lg, user_imput_ps), checkIflog(log_r, tk_screen)]
+                 )
+        config_btt = Button(tk_screen,
+                 borderwidth = 1,
+                 text ="Configurations",
+                 command = lambda:
+                 [closeScreen(tk_screen), tkinterBox("login")])
+        back_btt = Button(tk_screen,
+                 borderwidth = 1,
+                 text ="Go back",
+                 command = lambda:
+                 [loginBtt(user_imput_lg, user_imput_ps), checkIflog(log_r, tk_screen)]
+                 )
+        exit_btt = Button(tk_screen,
+                 borderwidth = 2,
+                 text ="Exit",
+                 command = lambda:
+                 [closeScreen(tk_screen), tkinterBox("login")])
+        widgets_list_reg = [m_lab_title, see_whoin_btt, config_btt, back_btt, line, exit_btt]
         callScreen(widgets_list_reg, tk_screen)
 
 def callScreen(widgets_s, tk):
