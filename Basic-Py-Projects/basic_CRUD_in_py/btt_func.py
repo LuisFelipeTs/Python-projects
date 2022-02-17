eb27fdb77079fc509e8eb95c63f7116988525d79
Luís Felipe Do Carmo Costa Tavares <48478593+LuisFelipeTs@users.noreply.github.com>
2022-02-17 16:12:07 -0300

    Login Screen commit(1.0) w log

--- Basic-Py-Projects/basic_CRUD_in_py/btt_func.py
+++ Basic-Py-Projects/basic_CRUD_in_py/btt_func.py
@@ -1,18 +1,24 @@
+from sre_parse import State
 from tkinter import *
 from validateRels import *
 
 def loginBtt(username_imp, password_imp, out_log):
-    out_log = out_log.get("1.0", "end-1c")
     username = str(username_imp.get("1.0", "end-1c"))
     password = str(password_imp.get("1.0", "end-1c"))
     lg_validation, lg_response = getLogin(username, password)
     if lg_validation:
-        print("lg")
+        changeLog(out_log, "Loged!")
     elif lg_response == "u":  
-        out_log.insert(END, "The Username is not in the database")
+        changeLog(out_log, "The Username is not in the database")
     elif lg_response == "p":
-        out_log.insert(END, "The Password is not correct")
+        changeLog(out_log, "The Password is not correct")   
  
+def changeLog(out_log, log_txt):
+    out_log["state"] = NORMAL
+    out_log.delete("1.0","end")
+    out_log.insert(END, log_txt)
+    out_log["state"] = DISABLED
+
 def readBtt():
     list_of_users = callAlluser()
     return list_of_users
