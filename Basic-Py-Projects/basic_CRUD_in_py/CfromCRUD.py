1cb4e67cf68a0bd01ed0cf5358cc0c95b1695628
Luís Felipe Do Carmo Costa Tavares <48478593+LuisFelipeTs@users.noreply.github.com>
2022-02-21 17:00:54 -0300

    Database(Xml) connected w gui

--- Basic-Py-Projects/basic_CRUD_in_py/CfromCRUD.py
+++ Basic-Py-Projects/basic_CRUD_in_py/CfromCRUD.py
@@ -1,6 +1,26 @@
-import User 
+
+import numpy as np
 import pandas as pd
 import RfromCrud
-user_data = pd.read_excel("user_data.xlsx")
-ux_guide = pd.read_excel("UX_guide.xlsx")
 
+user_data = pd.read_excel("data/user_data.xlsx")
+ux_guide = pd.read_excel("data/UX_guide.xlsx")
+
+def registerUser(user):
+    if RfromCrud.checkIfesxists(user.username): 
+        return False, "The Username is already in use"
+    new_u = pd.DataFrame([[
+        user.user_id,
+        user.name,
+        user.username,
+        user.password,
+        user.UX_]], columns=['user_id', 'name', 'username', 'password', 'UX'])
+    
+    union_frames = [user_data, new_u]
+    new_user_base = pd.concat(union_frames, sort = False, ignore_index= True)
+    
+    with pd.ExcelWriter("data/user_data.xlsx") as writer:
+        new_user_base.to_excel(writer)
+    return True, "Loged!"
+    
+    
