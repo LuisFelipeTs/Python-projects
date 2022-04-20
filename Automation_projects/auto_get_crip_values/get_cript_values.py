21e4d0836a9ac17776c646ac796225797e833900
Luís Felipe Do Carmo Costa Tavares <48478593+LuisFelipeTs@users.noreply.github.com>
2022-04-20 11:37:15 -0300

    Update get_cript_values.py

--- Automation_projects/auto_get_crip_values/get_cript_values.py
+++ Automation_projects/auto_get_crip_values/get_cript_values.py
@@ -1,7 +1,9 @@
-arq_cr_names = open('criptos_names.txt')
-arq_cr_names_ap = arq_cr_names.readlines()
+try:
+    arq_cr_names = open('criptos_names.txt')
+    arq_cr_names_ap = arq_cr_names.readlines()
 
-for i in range(len(arq_cr_names_ap)):
-    arq_cr_names_ap[i] = str(arq_cr_names_ap[i]).replace('\n', '')
-print(arq_cr_names_ap)
-
+    for i in range(len(arq_cr_names_ap)):
+        arq_cr_names_ap[i] = str(arq_cr_names_ap[i]).replace('\n', '')
+    print(arq_cr_names_ap)
+except:
+    print("a erro was found in the process")
