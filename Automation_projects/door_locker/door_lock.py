51f752bed4eef1836059fdfbe142f52bd07b405c
Luís Felipe Do Carmo Costa Tavares <48478593+LuisFelipeTs@users.noreply.github.com>
2022-05-22 17:42:12 -0300

    Update door_lock.py

--- Automation_projects/door_locker/door_lock.py
+++ Automation_projects/door_locker/door_lock.py
@@ -1,9 +1,15 @@
+from ast import Try
+import imp
 import time
 from tkinter import DISABLED, END, LEFT, X, Y, YES, Entry, Frame, Label, Button, Text, Tk, messagebox
+from urllib import response
 #import cam_res
+from whatsappComunicator import WhatsComunic
 import logging
+from alert import alert
+#import aws_connection
 logging.basicConfig(filename='logs\general_log.log', level=logging.DEBUG, format='%(asctime)s.%(msecs)03d %(levelname)s ====: %(message)s =;',
-    datefmt='%Y-%m-%d %H:%M:%S',)
+    datefmt='%Y-%m-%d %H:%M:%S')
 logging.info("Door's Lock simulator is ready to Start...")
 
 class doorLock():
@@ -13,7 +19,9 @@ class doorLock():
         self.visor_color = "white"
         self.security_pass = "54321"
         self.try_pass = 0
+        self.wtsapp_connect = WhatsComunic() 
         logging.debug("Initiating Door's Lock simulator...")
+        self.generateScreen()
 
     def generateScreen(self):
         self.tk_screen.geometry("200x190" )
@@ -35,7 +43,7 @@ class doorLock():
                                     command = lambda:[self.removeLastVisor()])
                 else:
                     btt_op = Button (self.btt_frame, text=char, bg = 'white',
-                                    command = lambda s = self, y = char: s.insertVisor(y))
+                                    command = lambda:[self.insertVisor(char)])
 
                 btt_op.pack (side=LEFT, expand=YES, fill = X)
             self.btt_frame.pack (expand=YES, fill = X)
@@ -58,37 +66,40 @@ class doorLock():
             
 
     def checkPassword(self):
-        #img_path, who, permition = cam_res.getPic()
-        permition = True
         if self.actual_pass == self.security_pass:
-            if permition == True:
+            #response1, response2 = cam_res.getPic(True)
+            response2 = True 
+            if response2 == True:
                 self.visor_color = 'green'
-                alert("Acesso Liberado!", "A sua entrada foi liberada com sucesso" , "info")
+                alert("Acesso Liberado!", "Bem vindo atual vendedor" , "info")
+                self.wtsapp_connect.wrMenss("Bem Vindo! Porta aberta.")
+                #aws_connection.sendImgS3(response1 , "Resident__")
                 logging.warning("Door's was opened by {}.".format("Owner"))
             else:
                 self.visor_color = 'yellow'
-                alert("Acesso Liberado!", "A sua entrada foi liberada com sucesso" , "info")
-                logging.warning("Door's was opened by {}.".format("Owner"))
-
+                alert("Acesso Postergado!", "Não o reconhecemos por favor aguarde a liberação pelo dono" , "info")
+                quest_req = self.wtsapp_connect.getResponse("Alguem que não reconhecemos com a senha está tentando acessar a porta, você deseja abrir a porta?\nResponda S - para Sim e N - para não")
+                if quest_req:
+                    logging.warning("Door's was opened by {}.".format("Visitant"))
+                    #aws_connection.sendImgS3(response1 , "Visitant")
+                else:
+                    logging.warning("Door's was protected from suspect {}.".format("Visitant"))
+                    #aws_connection.sendImgS3(response1 , "Suspect")
         else:
             alert("Acesso Bloqueado!", "A senha está incorreta!", "error")
             self.actual_pass = ""
+            self.visor.delete('1.0' , END)
             self.try_pass += 1
-            if self.try_pass == 3:
+            if self.try_pass == 4:
+                permition = True
+                #name_sus, suspect = cam_res.getPic(False)
+                self.wtsapp_connect.wrMenss("Cuidado, alguêm está tentando acessar a porta")
+                #aws_connection.sendImgS3(name_sus, "Suspect")
                 logging.warning("Someone is trying to acess your front door")
                 self.try_pass = 0
 
-
     def closeScreen(self):
         self.tk_screen.destroy()
-
-#https://www.foxinfotech.in/2019/01/alert-message-box-in-python-using-tkinter.html
-def alert(title, message, kind):
-    if kind not in ('error', 'warning', 'info'):
-        raise ValueError('Unsupported alert kind.')
-    show_method = getattr(messagebox, 'show{}'.format(kind))
-    show_method(title, message)
-#---------------------------------------------------------------------------------
 
 door_lock = doorLock()
  
